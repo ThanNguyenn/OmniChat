9bc3b09e6cee79218e135af6d0edd2fffc8c3b05
khoaNamK3 <namsugarcake@gmail.com>
2026-04-05 19:49:07 +0700

    add staff DashBoard DTO

---
+++ OmniChat.Infrastructure/Dtos/Responses/Staff/StaffDassboardResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniChat.Infrastructure.Dtos.Responses.Staff
+{
+    public class StaffDassboardResponse
+    {
+        public int TotalDoneTask { get; set; }
+
+        public int TotalCreateOrder { get; set; }
+
+        public double AfferageResolveTime { get; set; }
+
+        public double StaffPerformance { get; set; }
+
+    }
+}
