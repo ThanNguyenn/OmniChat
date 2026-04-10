b2c6409f01b1d554e46a7e23777fc06ee5297163
khoaNamK3 <namsugarcake@gmail.com>
2026-04-10 15:26:21 +0700

    TaskCancel Reason

---
+++ OmniChat.Application/Services/Implements/TaskCancelReasonService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using OmniChat.Application.Services.Interface;
+using OmniChat.Infrastructure.Persistence;
+using OmniChat.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniChat.Application.Services.Implements
+{
+    public class TaskCancelReasonService : BaseService<TaskCancelReasonService> , ITaskCancelReasonService
+    {
+        public TaskCancelReasonService(IUnitOfWork<OmniChatDbContext> unitOfWork, ILogger<TaskCancelReasonService> logger, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(unitOfWork, logger, mapper, httpContextAccessor)
+        {
+        }
+
+    }
+}
