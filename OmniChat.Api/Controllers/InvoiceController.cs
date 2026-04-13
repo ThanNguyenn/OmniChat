ca9a65038a1564ba1590dcf26c8b1195e2412677
Nguyễn Trí Hoàng Thân <156517391+ThanNguyenn@users.noreply.github.com>
2026-04-13 17:15:20 +0700

    Feat/get invoice (#91)
    * Add API for view Invoices
    
    Add API for view Invoices
    
    * Update InvoiceController.cs

--- OmniChat.Api/Controllers/InvoiceController.cs
+++ OmniChat.Api/Controllers/InvoiceController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting.Internal;
 using OmniChat.Api.Constants;
 using OmniChat.Application.Services.BackgroundJobs;
 using OmniChat.Application.Services.Interface;
 using OmniChat.Infrastructure.Dtos.Responses.Invoice;
 using OmniChat.Infrastructure.Metadatas;
+using OmniChat.Infrastructure.Models;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace OmniChat.Api.Controllers;
@@ -69,6 +69,8 @@ public class InvoiceController : BaseController<InvoiceController>
     }
 
     [HttpGet(ApiEndPointConstant.Invoice.ExportToExcel)]
+    [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
+
     public async Task<IActionResult> ExportToExcel([FromRoute] Guid id)
     {
         var templatePath = Path.Combine(_hostingEnvironment.WebRootPath, "templates", "SheetTemplate.xlsx");
@@ -80,4 +82,43 @@ public class InvoiceController : BaseController<InvoiceController>
         );
     }
 
+    [HttpGet(ApiEndPointConstant.Invoice.GetAll)]
+    [ProducesResponseType(typeof(ApiResponse<PagingResponse<GetInvoicesResponse>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+    [SwaggerOperation(
+        Summary = "Lấy danh sách Invoice",
+        Description = "Lấy danh sách Invoice có phân trang, và sắp xếp theo field chỉ định."
+    )]
+    public async Task<IActionResult> GetAllProducts([FromQuery] Guid? invoiceId, InvoiceStatus? status, int? pageNumber, int? pageSize, string? sortBy, bool? descending)
+    {
+        var result = await _invoiceService.GetInvoicesAsync(
+            invoiceId,
+            status,
+            pageNumber ?? 1,
+            pageSize ?? 10,
+            sortBy ?? "id",
+            descending ?? false
+            );
+        var response = ApiResponseBuilder.BuildResponse(StatusCodes.Status200OK, "Get all products successfully", result);
+        return StatusCode(StatusCodes.Status200OK, response);
+    }
+
+    [HttpGet(ApiEndPointConstant.Invoice.GetById)]
+    [ProducesResponseType(typeof(ApiResponse<PagingResponse<GetInvoicesResponse>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+    [SwaggerOperation(
+        Summary = "Lấy Invoice theo Id",
+        Description = "Lấy Invoice theo Id."
+    )]
+    public async Task<IActionResult> GetInvoiceById([FromRoute] Guid id)
+    {
+        var result = await _invoiceService.GetInvoiceAsync(id);
+        var response = ApiResponseBuilder.BuildResponse(StatusCodes.Status200OK, "Get invoice by id successfully", result);
+        return StatusCode(StatusCodes.Status200OK, response);
+
+    }
 }
---
+++ OmniChat.Infrastructure/Dtos/Responses/Invoice/GetInvoicesResponse.cs
@@ -0,0 +1,39 @@
+﻿using OmniChat.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniChat.Infrastructure.Dtos.Responses.Invoice;
+
+public class GetInvoicesResponse
+{
+    public Guid Id { get; set; }
+
+    public Guid CustomerId { get; set; }
+
+    public string CustomerName { get; set; }
+
+    public string CustomerPhoneNumber { get; set; }
+
+    public string CustomerEmail { get; set; }
+
+    public string CustomerAddress { get; set; }
+
+    public DateTime? StartedDate { get; set; }
+
+    public DateTime? EndedDate { get; set; }
+
+    public double Total { get; set; }
+
+    public InvoiceStatus InvoiceStatus { get; set; }
+
+    public InvoiceMethod InvoiceMethod { get; set; }
+
+    public DateTime? CompletedDate { get; set; }
+
+    public double PaidAmount { get; set; }
+
+    public double DeductedAmount { get; set; }
+}
