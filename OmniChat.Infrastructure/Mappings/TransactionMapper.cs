ad79846789adbcfd3741fcb7d6e249ec723e9f90
ThanNguyen <156517391+ThanNguyenn@users.noreply.github.com>
2026-04-15 16:33:42 +0700

    Wallet mapper

--- OmniChat.Infrastructure/Mappings/TransactionMapper.cs
+++ OmniChat.Infrastructure/Mappings/TransactionMapper.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using OmniChat.Infrastructure.Dtos.Responses.Transaction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace OmniChat.Infrastructure.Mappings;
 
-internal class TransactionMapper
+public class TransactionMapper : Profile
 {
+    public TransactionMapper()
+    {
+        CreateMap<Transaction, GetTransactionResponse>();
+    }
 }
--- OmniChat.Infrastructure/Mappings/WalletMapper.cs
+++ OmniChat.Infrastructure/Mappings/WalletMapper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using OmniChat.Infrastructure.Dtos.Responses.Wallet;
+using OmniChat.Infrastructure.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +9,13 @@ using System.Threading.Tasks;
 
 namespace OmniChat.Infrastructure.Mappings;
 
-internal class WalletMapper
+public class WalletMapper : Profile
 {
+    public WalletMapper()
+    {
+        CreateMap<Wallet, GetWalletResponse>()
+            .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount))
+            .ForMember(dest => dest.TotalDebt, opt => opt.Ignore())
+            .ForMember(dest => dest.Transactions, opt => opt.MapFrom(src => src.Transactions));
+    }
 }
