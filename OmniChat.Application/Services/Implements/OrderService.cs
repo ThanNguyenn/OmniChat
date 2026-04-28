ead04663ec48fcfbb04856529fe1a5d7240df9ee
khoaNamK3 <namsugarcake@gmail.com>
2026-04-29 01:06:36 +0700

    fix order submit

--- OmniChat.Application/Services/Implements/OrderService.cs
+++ OmniChat.Application/Services/Implements/OrderService.cs
@@ -543,7 +543,7 @@ public class OrderService : BaseService<OrderService>, IOrderService
             }
             if (order.Status != OrderStatus.Draft)
             {
-                throw new BusinessException("Chỉ các đơn hàng đang chờ xử lý mới có thể nộp.;
+                throw new BusinessException("Chỉ các đơn hàng đang chờ xử lý mới có thể nộp");
             }
             order.Status = OrderStatus.Pending;
             orderRepo.Update(order);
