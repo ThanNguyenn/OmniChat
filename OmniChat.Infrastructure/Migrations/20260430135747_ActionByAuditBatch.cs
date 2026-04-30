89b33e4c16e6c90c1db980396a42d85f3c5334f2
khoaNamK3 <namsugarcake@gmail.com>
2026-04-30 20:59:48 +0700

    Add Relation between staff (ActionBy) and AuditBatch

---
+++ OmniChat.Infrastructure/Migrations/20260430135747_ActionByAuditBatch.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmniChat.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActionByAuditBatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BatchAudits_Staffs_ActionById",
+                table: "BatchAudits");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BatchAudits_Staffs_ActionById",
+                table: "BatchAudits",
+                column: "ActionById",
+                principalTable: "Staffs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BatchAudits_Staffs_ActionById",
+                table: "BatchAudits");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BatchAudits_Staffs_ActionById",
+                table: "BatchAudits",
+                column: "ActionById",
+                principalTable: "Staffs",
+                principalColumn: "Id");
+        }
+    }
+}
