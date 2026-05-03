7c0cfaedfec0c81deff5b37ef1dc59a511a206c0
ThanNguyen <156517391+ThanNguyenn@users.noreply.github.com>
2026-05-03 22:25:15 +0700

    Update CreateProductRequest.cs

--- OmniChat.Infrastructure/Dtos/Requests/Product/CreateProductRequest.cs
+++ OmniChat.Infrastructure/Dtos/Requests/Product/CreateProductRequest.cs
@@ -12,24 +12,28 @@ namespace OmniChat.Infrastructure.Dtos.Requests.Product;
 public class CreateProductRequest : IValidatableObject
 {
     [Required(ErrorMessage = "Name là bắt buộc")]
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     public PackagingType ProductPackagingType { get; set; }
 
     public ProductKind ProductKind { get; set; }
 
+    [Required(ErrorMessage = "VolumeMl là bắt buộc")]
     [Range(1d, double.MaxValue, ErrorMessage = "VolumeMl phải >= 1")]
-    public double VolumeMl { get; set; }
+    public double? VolumeMl { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public Guid BrandId { get; set; }
+    [Required(ErrorMessage = "BrandId là bắt buộc")]
+    public Guid? BrandId { get; set; }
 
+    [Required(ErrorMessage = "Price là bắt buộc")]
     [Range(1d, double.MaxValue, ErrorMessage = "Price phải >= 1")]
-    public double Price { get; set; }
+    public double? Price { get; set; }
 
+    [Required(ErrorMessage = "LifeSpan là bắt buộc")]
     [Range(1, int.MaxValue, ErrorMessage = "LifeSpan phải >= 1")]
-    public int LifeSpan { get; set; }
+    public int? LifeSpan { get; set; }
 
     public IFormFile? Image { get; set; }
 
