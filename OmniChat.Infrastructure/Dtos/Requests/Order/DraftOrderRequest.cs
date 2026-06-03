fbcc3c6418dbd8ee72c8098ad4d478ab603c6573
ThanNguyen <156517391+ThanNguyenn@users.noreply.github.com>
2026-06-03 18:54:31 +0700

    happy pride month

--- OmniChat.Infrastructure/Dtos/Requests/Order/DraftOrderRequest.cs
+++ OmniChat.Infrastructure/Dtos/Requests/Order/DraftOrderRequest.cs
@@ -10,28 +10,27 @@ namespace OmniChat.Infrastructure.Dtos.Requests.Order;
 
 public class DraftOrderRequest : IValidatableObject
 {
-    [JsonIgnore]
     public Guid ConversationId { get; set; }
-
+    [JsonIgnore]
     public Guid CustomerId { get; set; }
-
-    [Required (ErrorMessage = "Messages là bắt buộc")]
+    [JsonIgnore]
+    //[Required (ErrorMessage = "Messages là bắt buộc")]
     public IEnumerable<string> Messages { get; set; }
-    //public IEnumerable<ValidationResult> Validate(ValidationContext context)
-    //{
-    //    if (ConversationId == Guid.Empty)
-    //    {
-    //        yield return new ValidationResult("ConversationId là bắt buộc",
-    //            new[] { nameof(ConversationId) });
-    //    }
-    //}
-
     public IEnumerable<ValidationResult> Validate(ValidationContext context)
     {
-        if (CustomerId == Guid.Empty)
+        if (ConversationId == Guid.Empty)
         {
-            yield return new ValidationResult("CustomerId là bắt buộc",
-                new[] { nameof(CustomerId) });
+            yield return new ValidationResult("ConversationId là bắt buộc",
+                new[] { nameof(ConversationId) });
         }
     }
+
+    //public IEnumerable<ValidationResult> Validate(ValidationContext context)
+    //{
+    //    if (CustomerId == Guid.Empty)
+    //    {
+    //        yield return new ValidationResult("CustomerId là bắt buộc",
+    //            new[] { nameof(CustomerId) });
+    //    }
+    //}
 }
